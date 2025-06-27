74675d650d39c0ea2c41b1f025c26ea5219e26a2
Hrishikesh Udawant <hrishiudawant7@gmail.com>
2025-06-27 18:58:59 +0530

    Fresh start

---
+++ Frontend/src/pages/Community/Community1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Community from "./Community";
+
+
+function Community1() { 
+  return (
+    <>
+    <Community />
+      
+    </>
+  );
+}
+
+export default Community1;
